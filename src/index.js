54d49447553c437fb6782b0e1c040b43633e44e6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-08-02 06:46:11 -0700

    Make the function to show error if any constraint is voilated by the email input

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 // index.js
 import "./styles.css";
 
-const dropDownButton = document.getElementById("dropdown-button");
+const form = document.querySelector("form");
+const email = document.getElementById("user-email");
+const emailError = document.querySelector("#user-email + span.email-error");
 
-const dropDownContent = document.getElementById("dropdown-content");
-
-dropDownButton.addEventListener("click", () => {
-    dropDownContent.classList.toggle("show");
+email.addEventListener("input", () => {
+  if (email.validity.valid) {
+    emailError.textContent = ""; 
+    emailError.className = "email-error"; 
+  } else {
+    showError();
+  }
 });
-window.addEventListener("click",(event) => {
-    if(!dropDownButton.contains(event.target)){
-        if(dropDownContent.classList.contains("show")){
-            dropDownContent.classList.remove("show");
-            
-        }
-    }
-})
+
+form.addEventListener("submit", (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+  emailError.className = "email-error active";
+}
+
   
